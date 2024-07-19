b1d91f06fc7a000a1866d39b9d630241545108ca
JosunHK <dennistai.mail@gmail.com>
2024-07-20 01:07:43 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+
+	PORT := os.Getenv("PORT")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(PORT))
+}
